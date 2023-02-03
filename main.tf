361230dd04905a4d1b190f0a9f022b11ed3a0a96
Vrashabh Sontakke <86019029+Vrashabh-Sontakke@users.noreply.github.com>
2023-02-03 15:56:02 +0530

    final modules commit

--- main.tf
+++ main.tf
@@ -2,11 +2,24 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+
+
+
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
         Name = "${var.env_prefix}-vpc"
     }
+}
+
+module "myapp-subnet-1" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
+    vpc_id = aws_vpc.myapp-vpc.id
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
 }
 
 resource "aws_default_security_group" "default-sg" {
@@ -40,7 +53,7 @@ resource "aws_default_security_group" "default-sg" {
 }
 
 /*
-resource "aws_security_group_rule" "web-http" {
+resource "aws_security_group_rule" "webb-http" {
   security_group_id = aws_vpc.myapp-vpc.default_security_group_id
   type              = "ingress"
   from_port         = 8080
@@ -72,13 +85,6 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-output "aws_ami_id" {
-    value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
-}
 
 resource "aws_key_pair" "ssh-key" {
     key_name = "server-key"
@@ -89,35 +95,16 @@ resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
 
-    subnet_id = aws_subnet.myapp-subnet-1.id
+    subnet_id = module.myapp-subnet-1.subnet.id
     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
-    # user_data = file("entry-script.sh")
+   user_data = file("entry-script.sh")
 
-    connection {
-        type = "ssh"
-        host = self.public_ip
-        user = "ec2-user"
-        private_key = file(var.private_key_location)
-    }
-
-    provisioner "file" {
-        source = "entry-script.sh"
-        destination = "/home/ec2-user/entry-script.sh"
-    }
-
-    provisioner "remote-exec" {
-        script = file("entry-script.sh")
-    }
-
-    provisioner "local-exec" {
-        command = "echo ${self.public_ip} > output-ip.txt"
-    }
-
+    
     tags = {
         Name = "${var.env_prefix}-server"
     }
--- vairables.tf
+++ vairables.tf
@@ -5,3 +5,4 @@ variable my_ip {}
 variable instance_type {}
 variable public_key_location {}
 variable private_key_location {}
+variable subnet_cidr_block {}
